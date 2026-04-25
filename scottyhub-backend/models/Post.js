8436fefdd971a1dfaac43369b28c7bc678bb98bd
scottyxcourage-cmyk <scottyxcourage@gmail.com>
2026-04-26 00:02:31 +0200

    Add files via upload

---
+++ scottyhub-backend/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    maxlength: 1000
+  },
+  mediaUrl: {
+    type: String,
+    default: ''
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    text: { type: String, maxlength: 500 },
+    createdAt: { type: Date, default: Date.now }
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Post', postSchema);
